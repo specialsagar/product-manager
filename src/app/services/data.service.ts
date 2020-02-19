7c744ea1130d7c10bacff21d4ee0413087d0f9e6
Sagar <specialsagar@gmail.com>
2020-02-19 12:17:47 +0530

    minor change

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const dataURL = 'http://localhost:8000'
+
 @Injectable({
   providedIn: 'root'
 })
-
-const dataURL = 'http://localhost:8000'
 export class DataService {
 
   constructor(private http: HttpClient) { }
